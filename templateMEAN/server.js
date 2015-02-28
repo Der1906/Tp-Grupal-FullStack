8cb72689e5b6e280572bb602569e5221060857f7
kanaima <ferrarijuanp@gmail.com>
2015-02-28 17:46:14 -0300

    dependencias cambiadas

--- templateMEAN/server.js
+++ templateMEAN/server.js
@@ -1,21 +1,29 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var app = express();
 var port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:3600/meandb');
+//mongoose.connect('mongodb://localhost:3600/meandb');
 
-app.configure(
-	function(){
-		app.use(express.static(__dirname+'/angular'));
-		app.use(express.logger('dev'));
-		app.use(express.urlencoded())
-		app.use(express.json())
-		app.use(express.methodOverride());
-	}
-);
+
+//app.configure(
+//	function(){
+		app.use(express.static(__dirname + '/angular'));
+		//app.use(express.logger('dev'));
+		//app.use('/angular', express.static(path.join(__dirname, '/angular/')));
+		app.use(bodyParser.json());
+		//app.use(express.json())
+		//app.use(express.methodOverride());
+//	}
+//);
 
 //require('./app/routes.js')(app);
+
+//var route = express.Router();
+
+
 
 app.listen(port);
 console.log("app por el puerto "+ port);
