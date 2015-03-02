31361371ba46837cff3ed8cd1bbb5a449c2bf6bc
Emilio Albano Llames <ellames@gmail.com>
2015-03-02 10:30:48 -0300

    Actualización del archivo server.js
    Se cambio el drive de conexión a la base de datos de "Mongoose" por la versión nativa.

--- templateMEAN/server.js
+++ templateMEAN/server.js
@@ -2,12 +2,18 @@ var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
     methodOverride  = require("method-override"),
-    mongoose        = require('mongoose');
+    mongodb     = require('mongodb').MongoClient;
 var router = express.Router();
 var port = process.env.PORT || 3000;
 
-// Conexion a Mongoose
-mongoose.connect('mongodb://localhost/meanDB', function(err, res) {
+// propiedades de la conexion a la base de datos
+var db_host = "127.0.0.1";
+var db_port = "27017";
+var db_name = "mongodb";
+var db_string = "mongodb://".concat(db_host,":",db_port,"/",db_name);
+
+// Conexion a MongoDB
+mongodb.connect(db_string, function(err, res) {
   if(err) throw err;
   console.log('Connected to Database');
  });
